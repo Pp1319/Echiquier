85adb8f5858014e6755272bf7d1aefaacf498ef9
CDA <CDA@DL14.exchange.ad.afpanet>
2020-09-22 17:34:56 +0200

    Ajoutez des fichiers projet.

---
+++ Echiquier/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Echiquier
+{
+    partial class Echiquier
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Veuillez saisir le nombre de lignes et de colonnes :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.Saisie);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(61, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 334);
+            this.panel1.TabIndex = 3;
+            
+            // 
+            // Echiquier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Echiquier";
+            this.Text = "Echiquier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Echiquier/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace Echiquier
+{
+    public partial class Echiquier : Form
+    {
+        public Echiquier()
+        {
+            InitializeComponent();
+        }
+
+        private void Saisie(object sender, EventArgs e)
+        {
+            int x = 8;
+            int y = 8;
+            Button bouton1 = new Button();
+            bouton1.Location = new Point(0, 0);
+            bouton1.Size = new Size(65, 65);
+            bouton1.BackColor = Color.White;
+
+            /*for (int nblignes = 0; nblignes.Length; nblignes++)
+            {
+                Button bouton = new Button();
+                for (int nbcolonnes = 0; nbcolonnes.Length; nbcolonnes++)
+                {
+
+                }
+            }*/
+        }
+
+       
+    }
+}
